3a46359a27e31730a069a5a9821548749684d01d
Ashwini Mane <104590017+amfullstack1985@users.noreply.github.com>
2022-08-02 11:44:24 +0530

    Create Student.py

---
+++ Student.py
@@ -0,0 +1,11 @@
+class Student(self):
+  def __init__(self,roll,name,per):
+    roll=self.roll
+    name=self.name
+    per=self.per
+  def display(self):
+    print("Roll No:",self.roll)
+    print("Name:",self.name)
+    print("Percentage:",self.per)
+s1=Student(1,"ashwini",89)
+s1.display()
