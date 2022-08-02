d291a8d49a7e25ffeae1e90f3a9d5bc7b86780b0
Ashwini Mane <104590017+amfullstack1985@users.noreply.github.com>
2022-08-02 11:23:04 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ class Student
   public static void main(String args[])
   {
     System.out.println("Hello Java");
+    System.out.println("This is java program written in Ecommerce Repository");
   }
 }
